5f2ac7cf6421bfd7797051552941372425cacf8f
junn999 <junioralberto1421@gmail.com>
2024-05-23 09:44:38 -0600

    Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppPruebas',
+  webDir: 'www'
+};
+
+export default config;
